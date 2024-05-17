bc85ebb1ee8dd94e11422b38353e074ea237f7e9
LeoniTreis <166816674+LeoniTreis@users.noreply.github.com>
2024-05-17 18:27:11 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ basic.forever(function () {
     calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
     calliBot2.setRgbLed1(C2RgbLed.All, 0x00ffdc, 8)
     calliBot2.motor(C2Motor.beide, C2Dir.rueckwaerts, 50)
-    calliBot2.warte(C2SensorWait.distanceCm, C2Check.equal, 10)
+    calliBot2.warte(C2SensorWait.distanceCm, C2Check.greaterThan, 10)
     calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
     calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, 50)
     calliBot2.motor(C2Motor.rechts, C2Dir.rueckwaerts, 50)
